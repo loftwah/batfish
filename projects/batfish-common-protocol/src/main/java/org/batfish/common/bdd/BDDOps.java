ffd764197589b875dafddeb991d42ea404d0ebc3
Matt Brown <matt@intentionet.com>
2019-03-01 15:27:34 -0800

    BDD fastOr

--- projects/batfish-common-protocol/src/main/java/org/batfish/common/bdd/BDDOps.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/common/bdd/BDDOps.java
@@ -2,11 +2,18 @@ package org.batfish.common.bdd;
 
 import static com.google.common.base.MoreObjects.firstNonNull;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import net.sf.javabdd.BDD;
 import net.sf.javabdd.BDDFactory;
+import net.sf.javabdd.JFactory;
 
 public final class BDDOps {
+  private static final Field BDD_INDEX_FIELD = bddIndexField();
+  private static final Method BDD_ADD_REF_METHOD = bddAddRefMethod();
+
   private final BDDFactory _factory;
 
   public BDDOps(BDDFactory factory) {
@@ -39,6 +46,69 @@ public final class BDDOps {
   /** Returns bdd.not() or {@code null} if given {@link BDD} is null. */
   public static BDD negateIfNonNull(BDD bdd) {
     return bdd == null ? bdd : bdd.not();
+  }
+
+  private static Class<?> bddClass() {
+    return Arrays.stream(JFactory.class.getDeclaredClasses())
+        .filter(cls -> cls.getSimpleName().equals("bdd"))
+        .findFirst()
+        .get();
+  }
+
+  private static Field bddIndexField() {
+    Class<?> bddClass = bddClass();
+    try {
+      Field field = bddClass.getDeclaredField("_index");
+      field.setAccessible(true);
+      return field;
+    } catch (NoSuchFieldException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static Method bddAddRefMethod() {
+    try {
+      Method method = JFactory.class.getDeclaredMethod("bdd_addref", int.class);
+      method.setAccessible(true);
+      return method;
+    } catch (NoSuchMethodException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static int getBddIndex(BDD bdd) throws IllegalAccessException {
+    return (Integer) BDD_INDEX_FIELD.get(bdd);
+  }
+
+  private static void setBddIndex(BDD bdd, int index) throws IllegalAccessException {
+    BDD_INDEX_FIELD.set(bdd, index);
+  }
+
+  private void addRef(int bddId) throws InvocationTargetException, IllegalAccessException {
+    BDD_ADD_REF_METHOD.invoke(_factory, bddId);
+  }
+
+  public BDD orWithId(BDD... disjuncts) {
+    BDD result = _factory.zero();
+    for (BDD disjunct : disjuncts) {
+      result.orWith(disjunct.id());
+    }
+    return result;
+  }
+
+  public BDD fastOr(BDD... disjuncts) {
+    try {
+      BDD result = _factory.zero();
+      for (BDD disjunct : disjuncts) {
+        int index = getBddIndex(disjunct);
+        addRef(index);
+        result.orWith(disjunct);
+        setBddIndex(disjunct, index);
+      }
+      return result;
+    } catch (IllegalArgumentException | IllegalAccessException | InvocationTargetException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   public BDD or(BDD... disjuncts) {
--- projects/batfish-common-protocol/src/test/java/org/batfish/common/bdd/BDDOpsTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/common/bdd/BDDOpsTest.java
@@ -2,8 +2,10 @@ package org.batfish.common.bdd;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
+import java.util.Random;
 import net.sf.javabdd.BDD;
 import net.sf.javabdd.BDDFactory;
 import net.sf.javabdd.JFactory;
@@ -116,5 +118,73 @@ public class BDDOpsTest {
     BDD var1 = _factory.ithVar(0);
     BDD var2 = _factory.ithVar(1);
     assertThat(BDDOps.orNull(null, var1, var2), equalTo(var1.or(var2)));
+  }
+
+  @Test
+  public void benchmarkOr() {
+    Random random = new Random();
+
+    int numDisjuncts = 2000;
+    int numRounds = 60; // how many different sets of disjuncts
+    int numIters = 50; // how many times we or each set of disjuncts
+
+    long orTime = 0;
+    long fastOrTime = 0;
+    long orWithIdTime = 0;
+
+    for (int round = 0; round < numRounds; round++) {
+      // reset timers after warmup
+      if (round == 10) {
+        orTime = 0;
+        fastOrTime = 0;
+        orWithIdTime = 0;
+      }
+
+      // reinitialize bdd factory
+      init();
+      _factory.setVarNum(32);
+      BDDInteger bddInteger = BDDInteger.makeFromIndex(_factory, 32, 0, false);
+
+      System.gc();
+
+      BDD[] disjuncts = new BDD[numDisjuncts];
+      for (int i = 0; i < numDisjuncts; i++) {
+        disjuncts[i] = bddInteger.value(random.nextInt(2 ^ 32));
+      }
+
+      BDD or1 = null;
+      {
+        long t = System.currentTimeMillis();
+        for (int i = 0; i < numIters; i++) {
+          or1 = _bddOps.or(disjuncts);
+        }
+        orTime += System.currentTimeMillis() - t;
+      }
+
+      BDD or2 = null;
+      {
+        long t = System.currentTimeMillis();
+        for (int i = 0; i < numIters; i++) {
+          or2 = _bddOps.fastOr(disjuncts);
+        }
+        fastOrTime += System.currentTimeMillis() - t;
+      }
+
+      BDD or3 = null;
+      {
+        long t = System.currentTimeMillis();
+        for (int i = 0; i < numIters; i++) {
+          or3 = _bddOps.orWithId(disjuncts);
+        }
+        orWithIdTime += System.currentTimeMillis() - t;
+      }
+
+      assertEquals(or1, or2);
+      assertEquals(or1, or3);
+    }
+
+    System.out.println(String.format("or=%d", orTime));
+    System.out.println(String.format("orWithId=%d", orWithIdTime));
+    System.out.println(String.format("fastOr=%d", fastOrTime));
   }
 }
